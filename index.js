2558d5ac410912370892695c6c5583ee1e6cc813
devlogix <email>
2025-06-02 17:24:30 +0530

    port correct database

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log("Database is connected");
+    console.log("Database is connected on live");
   })
   .catch((err) => {
     console.log(err);
